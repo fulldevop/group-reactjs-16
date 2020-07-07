135229cba7515e8fe95a5d8b8b0f7501b57fcb87
Анастасия Ратникова <ratnikova@techart.ru>
2020-07-07 13:56:51 +0300

    les-7.1

--- Ratnikova_Anastasiya/src/store.js
+++ Ratnikova_Anastasiya/src/store.js
@@ -1,8 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import {createLogger} from 'redux-logger';
 import {composeWithDevTools} from 'redux-devtools-extension';
+
 import {routerMiddleware} from 'connected-react-router';
 import {createBrowserHistory} from 'history';
+
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //localStorage
 
 import {initReducer} from 'reducers';
 import {loggerMiddleware} from 'middlewares/logger';
@@ -11,12 +15,38 @@ import {fireChatMiddleware} from 'middlewares/fireChat';
 
 export const history = createBrowserHistory();
 
-export const store = createStore(initReducer(history), composeWithDevTools(
+/*export const store = createStore(initReducer(history), composeWithDevTools(
     applyMiddleware(
         createLogger(),
         // loggerMiddleware,
         botMiddleware,
-        fireChatMiddleware,
+        // fireChatMiddleware,
         routerMiddleware(history)
     )
-));
+));*/
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['chats'],
+};
+
+export function initStore(){
+    const initialStore = {};
+
+    const store = createStore(
+        persistReducer(persistConfig, initReducer(history)),
+        initialStore,
+        composeWithDevTools(
+            applyMiddleware(
+                routerMiddleware(history),
+                createLogger(),
+                // loggerMiddteware,
+                botMiddleware,
+                /*apiMiddleware,
+                thunk,*/
+            )));
+
+    const persistor = persistStore(store);
+    return {store, persistor};
+}
